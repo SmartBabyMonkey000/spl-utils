5addfb3d9e95f2a80d33974029928dd587947833
smartbabymonkey000 <smartbabymonkey000@gmail.com>
2024-08-02 10:41:10 +0900

    modify getAllTokens function

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
-const { getAllTokens } = require('./utils')
+const { getAllTokens, sleep } = require('./utils')
 dotenv.config();
 
 const RPC_URL = process.env.RPC_URL;
 
 async function main() {
-    const walletAddress = '9EVexPCR29L9s4edQgkBEcsaQp7gyc4ueZcYMuKzUqk3';
+    const walletAddress = 'MoM6ZYyWsDSoYCa2TiXXwsMS7vBGztxNjr9HGNoRZqM';
     while(1) {
+        sleep(5000);
         await getAllTokens(RPC_URL, walletAddress);
     }
     
